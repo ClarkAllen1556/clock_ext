fcc0d5076e04c21c77f4d4adc8006d96f58affde
Allen <allenclark@u.boisestate.edu>
2020-04-20 14:19:59 +0900

    starting styling functions

--- ts/popup.ts
+++ ts/popup.ts
@@ -1,12 +1,12 @@
 import { Clock } from "./Clock.js";
 import { TIME_FORMAT } from "./enum.js";
 
-const clockEL = document.createElement("h1");
-const clock = new Clock(new Date());
+const clockEL = document.createElement("div");
 
-setInterval( () => {
+setInterval(() => {
+  const clock = new Clock(new Date());
+
   clockEL.innerText = clock.getFormattedTime(TIME_FORMAT.HH_MM_SS_24);
-
   window.console.log(clockEL.innerText);
 
   document.getElementById("time-placeholder")?.appendChild(clockEL);
