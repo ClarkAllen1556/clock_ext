8bb34862f824882f11cf04cd57c2c51fa6d05a80
Allen <allenclark@u.boisestate.edu>
2020-04-21 11:24:45 +0900

    Popup styling and options page match ff-photon more now.

--- ts/options.ts
+++ ts/options.ts
@@ -1,6 +1,9 @@
 document.getElementById("time-format-radio")?.addEventListener("submit", saveOptions);
+document.getElementById("time-format-radio")?.addEventListener("click", toggleButton);
 
-function saveOptions(e : Event ) {
+let saved : boolean;
+
+function saveOptions(e : Event) {
   const formOptions = document.getElementsByName("time-format");
   let settings;
 
@@ -13,8 +16,42 @@ function saveOptions(e : Event ) {
 
   browser.storage.local.set({
     TIME_FORMAT: settings?.TIME_FORMAT
-  });
+  }).then(resp => {
+    saved = true;
+    toggleButton();
 
-  document.getElementById("time-format-radio")?.append("Settings saved!");
+    if (!document.getElementById("saved-notice")) {
+      const savedNotice = document.createElement("span");
+      savedNotice.setAttribute("id", "saved-notice");
+      savedNotice.innerText = "Settings saved!";
+      savedNotice.style.opacity = "1";
+
+      document.getElementById("time-format-radio")?.append(savedNotice);
+    }
+
+    setTimeout(() => {
+      document.getElementById("saved-notice")?.remove();
+    }, 3500)
+  }).catch(e => {
+    saved = false;
+    window.console.error("SETTINGS COULD NOT BE SAVED: " + new Error(e));
+  })
+
   e.preventDefault();
+}
+
+function toggleButton() {
+  console.log(saved);
+  const btn: HTMLElement = document.getElementById("save-btn")!;
+
+  if (saved){
+    (btn as HTMLInputElement).disabled = true;
+    btn.setAttribute("class", "disabled");
+  }
+  else{
+    (btn as HTMLInputElement).disabled = false;
+    btn.setAttribute("class", "enabled");
+  }
+
+  saved = false;
 }
--- ts/popup.ts
+++ ts/popup.ts
@@ -2,15 +2,13 @@ import { Clock } from "./Clock.js";
 
 const clockEL = document.createElement("div");
 const placeEL = document.getElementById("time-placeholder");
-
-placeEL?.setAttribute("style", "font-size: 50px");
+const clock = new Clock(new Date());
+let userSettings : any;
 
 setInterval(sendTime, 500);
 
 async function sendTime() {
-    const userSettings : any = await browser.storage.local.get("TIME_FORMAT");
-
-    const clock = new Clock(new Date());
+    userSettings = await browser.storage.local.get("TIME_FORMAT");
     clockEL.innerText = clock.getFormattedTime(userSettings.TIME_FORMAT);
     placeEL?.appendChild(clockEL);
 }
